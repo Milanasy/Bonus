6213a6505b983489db2fc40ca389793c482cf804
Anastasiia Rakhmanova <rar.anastasiya@gmail.com>
2023-06-03 12:50:12 +0300

    correction

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,18 @@ public class Main {
         // Начальная сумма
         int startingScore = 50;
         // Сумма пополнения
-        int replenishtAmount = 100;
+        int replenishtAmount = 1100;
         // Общая сумма с пополнением
         int result = startingScore + replenishtAmount;
 
         if (replenishtAmount >= 1000) {
-            System.out.println("Сумма пополнения больше или равна 1000");
+            System.out.println("Сумма пополнения: " + replenishtAmount);
             System.out.println("Сумма на балансе: " + result);
             System.out.println("Начисленно бонусов: " + replenishtAmount / 10);
         } else {
-            System.out.println("Сумма пополнения меньше 1000");
+            System.out.println("Сумма пополнения: " + replenishtAmount);
             System.out.println("Сумма на балансе: " + result);
             System.out.println("Начисленно бонусов: 0");
-        }
-
-        System.out.println();
-
-        replenishtAmount = 1200;
-        result = startingScore + replenishtAmount;
-
-        if (replenishtAmount >= 1000) {
-            System.out.println("Сумма пополнения больше или равна 1000");
-            System.out.println("Сумма на балансе: " + result);
-            System.out.println("Начисленно бонусов: " + replenishtAmount / 10);
         }
     }
 }
